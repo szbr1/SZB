cf9fe2e28db52cdf559e41174fcf44d2cfd03a00
szbr1 <szb8480@gmail.com>
2024-12-09 15:52:27 +0500

    Updated next.config.ts and package.json

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/develop.ts',  // Replace with your repository name
-  assetPrefix: '/develop.ts/',  // Ensure assets load correctly from the repository path
-
   reactStrictMode: true,
 };
 
