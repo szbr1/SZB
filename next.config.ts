ba79546f506c99950bee404b9641b8ebe6ce4968
szbr1 <szb8480@gmail.com>
2024-12-09 13:19:38 +0500

    Fix next.config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Set the basePath and assetPrefix for GitHub Pages
-  basePath: '/website',  // Replace with your repo name
-  assetPrefix: '/website/', // Ensure assets load correctly from GitHub Pages
-
   // Optional: enable React Strict Mode and other configurations
   reactStrictMode: true,
 };
