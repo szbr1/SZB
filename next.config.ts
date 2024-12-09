4e3c465c6151f76c8f8cb49dc17215990c4e6745
szbr1 <szb8480@gmail.com>
2024-12-09 13:48:13 +0500

    Update next.config.ts for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Optional: enable React Strict Mode and other configurations
+  basePath: '/develop.ts',  // Replace with your repository name
+  assetPrefix: '/develop.ts/',  // Ensure assets load correctly from the repository path
+
   reactStrictMode: true,
 };
 
